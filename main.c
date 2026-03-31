986e938ce353ddda99b6899ad3c8ab1d42a9e202
Zainal <zainalsekip1122@gmail.com>
2026-03-31 20:33:38 +0700

    tambah tampilan menu utama

--- main.c
+++ main.c
@@ -1,7 +1,97 @@
 #include <stdio.h>
 
+// deklarasi fungsi
+void konversi_suhu();
+void konversi_jarak();
+void konversi_berat();
+void konversi_waktu();
+
 int main() {
-    printf("=== KALKULATOR KONVERSI ===\n");
-    printf("===============================")
+    int pilih;
+
+    do {
+        printf("\n=====================================\n");
+        printf("   KALKULATOR KONVERSI SATUAN CLI\n");
+        printf("=====================================\n");
+        printf("1. Konversi Suhu\n");
+        printf("2. Konversi Jarak\n");
+        printf("3. Konversi Berat\n");
+        printf("4. Konversi Waktu\n");
+        printf("0. Keluar\n");
+        printf("=====================================\n");
+        printf("Pilih menu: ");
+        scanf("%d", &pilih);
+
+        switch(pilih) {
+            case 1: konversi_suhu(); break;
+            case 2: konversi_jarak(); break;
+            case 3: konversi_berat(); break;
+            case 4: konversi_waktu(); break;
+            case 0:
+                printf("\nTerima kasih sudah menggunakan program!\n");
+                break;
+            default:
+                printf("\nPilihan tidak valid!\n");
+        }
+
+    } while(pilih != 0);
+
     return 0;
 }
+
+// ==============================
+// FUNGSI-FUNGSI
+// ==============================
+
+// Fungsi untuk konversi suhu
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Fungsi untuk konversi jarak
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Fungsi untuk konversi berat
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Fungsi untuk konversi waktu
+
+
+
+
+
+
+
+
+
